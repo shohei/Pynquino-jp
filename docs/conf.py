9b3764b90235e7eb4533e6a7bb61fcdb27dbf474
Shohei Aoki <shoaok@gmail.com>
2018-05-18 15:22:35 +0900

    comment out nbextension

--- docs/conf.py
+++ docs/conf.py
@@ -159,9 +159,9 @@ texinfo_documents = [
      'Miscellaneous'),
 ]
 
-extensions = [
-    'nbsphinx',
-    'sphinx.ext.mathjax',
-]
-exclude_patterns = ['_build', '**.ipynb_checkpoints']
-
+# extensions = [
+#     'nbsphinx',
+#     'sphinx.ext.mathjax',
+# ]
+# exclude_patterns = ['_build', '**.ipynb_checkpoints']
+ 
